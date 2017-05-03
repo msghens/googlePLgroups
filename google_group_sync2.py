8bc32eb59efd0ac8c0cadae58b32c755d985a087
Michael Ghens <mghens@sbcc.edu>
2017-05-03 10:30:38 -0700

    Fix hardcode for term 201810

--- google_group_sync2.py
+++ google_group_sync2.py
@@ -1,26 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-#  sbcc_maillists.py
-#  
-#  Copyright 2016 Michael Ghens <mghens@sbcc.edu>
-#  
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#  
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#  
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA 02110-1301, USA.
-#  
-#  
+
 
 # Imports (cuz we like them on top)
 import sys
@@ -210,7 +190,8 @@ UNION
 SELECT gzf_get_id(sirasgn_pidm,'USERID')
   || '@pipeline.sbcc.edu' AS EMAIL_ADDRESS
 FROM sirasgn
-WHERE sirasgn_term_code= szf_get_this_soaterm()
+WHERE --sirasgn_term_code= szf_get_this_soaterm()
+sirasgn_term_code= '201810'
 AND sirasgn_crn        ='%s'
 		""" % (course.split('.')[0],course.split('.')[0])
 		
